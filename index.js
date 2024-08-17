963d3f2bc5c2ea67aef52815da8e0bac15e99c1c
sarajit <bdsarajit499@gamil.com>
2024-08-17 16:29:51 +0600

    added like like

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
      'http://localhost:5174',
+     'https://programming-hero-task-d058d.web.app'
     ],
   credentials: true,
   optionSuccessStatus: 200,
